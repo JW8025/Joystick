25c2b6e04ce32a89599a6b9453cdac2a28e9bbe5
LikeKIT <k.i.tayupov@mail.ru>
2016-06-12 17:21:14 +0300

    resize radius /// add icon

--- app/src/main/java/ru/tayupov/kamil/joystick/MainActivity.java
+++ app/src/main/java/ru/tayupov/kamil/joystick/MainActivity.java
@@ -1,9 +1,11 @@
 package ru.tayupov.kamil.joystick;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
